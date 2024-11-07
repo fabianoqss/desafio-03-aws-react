ecd0f1cc18ea64d5a8f4512ae2e35210f55afe9f
Fabiano <fabianoquirinos@outlook.com>
2024-11-07 15:01:53 -0300

    Feat: Iniciando estilização: Minha História

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { IoEnterOutline } from "react-icons/io5";
 
 const Header: React.FC = () =>{
   return (
-    <header className="bg-dark_green text-white p-4 fixed rounded-b-3xl w-full">
+    <header className="bg-dark_green text-white p-4  rounded-b-3xl w-full">
     <nav className="flex justify-between items-center w-full">
       <div className="flex justify-center gap-8 w-full">
         <a href="#inicio" className="text-2xl font-semibold hover:text-primary_color">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,32 @@ const Home: React.FC = () => {
 
 
   return (
-    <div>
+    <div className='bg-secondary_text'>
       <Header />
+
+    <section className='grid grid-cols-2 items-center justify-items-center mt-28'>
+      <div className='grid gap-4'>
+      <img src="" alt="" className='w-[260px] h-[260px] bg-black rounded-full'/>
+      <h1 className='text-7xl'>Patoxxx</h1>
+      <p className='text-3xl'>Paraíba, SP</p>
+      <h1 className='text-3xl'>felipepatoxx@gmail.com</h1>
+      </div>
+
+    <div className='grid gap-8'>
+    <h1 className='text-7xl'>Hello,<br /> I'm Felipe Pato</h1>
+    <p>Olá, meu nome é Felipe Pato e sou dev<br />
+      há 24 anos, sou um senior experiente e <br />
+      potente, sempre disposto a evoluir!</p>
+
+
+    </div>
+    </section>
+
+
+
+
+
+
     </div>
   );
 };
