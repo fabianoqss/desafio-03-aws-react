bc6a3ea70eeaa3c2131ca6224c73bd802f7e9347
Fabiano <fabianoquirinos@outlook.com>
2024-11-07 15:25:32 -0300

    Feat: Terminando Estilizacao da minha historia

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,6 +28,12 @@ const Home: React.FC = () => {
     </section>
 
 
+    <article className='bg-card_color max-w-[1240px] mx-auto rounded-3xl mt-36 p-16'>
+    <h1 className='text-7xl text-white '>Minha História</h1>
+    <p className='text-white mt-16'>Olá, eu sou Felipe Pato e comecei minha carreira trabalhando em um pequeno escritório na California  para meu chefe Elon Musk, fui demitido 2 dias depois por commitar errado e apagar o repositório da  empresa. Voltei então a trabalhar de casa criando sites de Yoga para idosos, ganhando muito dinheiro  no processo, esses sites eram desenvolvidos usando Fortro e Cobol, ao qual aprendi com meu tio  Jhon. No entanto, essa forma de fazer sites não durou muito com o advento das IAs, foi então que  decidi estudar muito e acabei passando no processo seletivo da cicada 3301, me tornando assim um  grande dev da minha area, hoje trabalho espionando hackers e destruindo governos, eu sou Felipe Pato!</p>
+
+    </article>
+
 
 
 
