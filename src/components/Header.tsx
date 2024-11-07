d01e9733e657d5a8d279b5b00fa9623f4316058d
Fabiano <fabianoquirinos@outlook.com>
2024-11-07 09:22:19 -0300

    Feat: Finalização da estilização do Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { IoEnterOutline } from "react-icons/io5";
 
 
 const Header: React.FC = () =>{
@@ -7,20 +7,20 @@ const Header: React.FC = () =>{
     <header className="bg-dark_green text-white p-4 fixed rounded-b-3xl w-full">
     <nav className="flex justify-between items-center w-full">
       <div className="flex justify-center gap-8 w-full">
-        <a href="#inicio" className="text-lg font-semibold hover:text-primary_color">
+        <a href="#inicio" className="text-2xl font-semibold hover:text-primary_color">
           Início
         </a>
-        <a href="#historia" className="text-lg font-semibold hover:text-primary_color">
+        <a href="#historia" className="text-2xl font-semibold hover:text-primary_color">
           Minha História
         </a>
-        <a href="#experiencia" className="text-lg font-semibold hover:text-primary_color">
+        <a href="#experiencia" className="text-2xl font-semibold hover:text-primary_color">
           Experiências
         </a>
-        <a href="#contato" className="text-lg font-semibold hover:text-primary_color">
+        <a href="#contato" className="text-2xl font-semibold hover:text-primary_color">
           Contato
         </a>
       </div>
-      <a href="#entrar" className="text-lg font-semibold hover:text-primary_color ml-auto">
+      <a href="#entrar" className="flex items-center gap-3 text-2xl font-semibold hover:text-primary_color  mr-2"> <IoEnterOutline className='h-8 w-8 mt-0.5'/>
         Entrar
       </a>
     </nav>
