64241d06d7a7db02dc2a339317768e4cf214e398
Fabiano <fabianoquirinos@outlook.com>
2024-11-10 11:08:33 -0300

    Feat: Mudanças no arquivo de configuração

--- src/Firebase/firebaseConfig.ts
+++ src/Firebase/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth, GithubAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWVP8u8aVIrJlSh57uRZanPV9Czhkl97w",
-  authDomain: "project-6f4cc.firebaseapp.com",
-  projectId: "project-6f4cc",
-  storageBucket: "project-6f4cc.firebasestorage.app",
-  messagingSenderId: "7034155123",
-  appId: "1:7034155123:web:a2f0ed3ae146068450fa06",
-  measurementId: "G-TKBX6Q6DQ1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!getApps().length) {
